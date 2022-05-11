6ddbec0912434585db6b9fed7f48bbb714224691
Amilcar Pio <amilcar.pio@gmail.com>
2022-05-11 18:40:04 -0300

    homeupdate

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-      <router-view></router-view>
+      <router-view>
+        <Home />
+      </router-view>
 </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/page/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   },
 
   data() {
@@ -29,7 +31,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'KoHo', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- front/src/components/page/Home.vue
+++ front/src/components/page/Home.vue
@@ -1,22 +1,41 @@
 <template>
-  <div class="hello">
-     <h1> Home do Amilca</h1>
-
-      <label>Mesa</label>
-      <input class="" v-model="mesa">
-      <br>
-      <br>
-      <div @click="openCardapio" class="card">
-        <div class="card-body">
-          Ir para o Cardapio
-        </div>
-      </div>
+<!-- header -->
+<section class="header">
+  <div class="container">
+    <h1 class="title-1">MEU</h1>
+    <h1 class="title-2">MENU</h1>
+    <p class="sub-title lead text-center">Bem vindo(a), {{user}} leia o QR CODE
+    da sua mesa ou digite o número da sua mesa abaixo:</p>
   </div>
+</section>
+<!-- input mesa -->
+  <div class="container">
+    <div class="d-flex justify-content-center">
+      <div class="input-group mesa-input">
+        <input type='number' min="1" max="99" class="form-control" v-model="mesa">
+      </div>
+    </div>
+  </div>
+<!-- buttons -->
+<section class="buttons-section">
+  <div class="container seila">
+    <div class="container background p-2 pb-3 mt-2">
+      <div class="container">
+        <button type="button" @click="openCardapio" class="btn mt-2 " >ESCANEAR QR CODE</button>
+      </div>
+
+      <div class="container">
+        <button type="button" @click="openCardapio" class="btn mt-2" >RESTAURANTES</button>
+      </div>
+    </div>
+  </div>
+</section>
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Home',
   data() {
       return {
         mesa: "",
@@ -40,6 +59,74 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+body{
+  background-color: #FCE83A;
+}
+@media (max-width: 720px){
+  .mesa-input{
+    width: 100%;
+  }
+
+  .btn{
+    width: 100%;
+  }
+}
+
+@media (min-width: 720px){
+  .mesa-input{
+    width: 30%;
+  }
+
+  .btn{
+    width: 100%;
+  }
+
+  .seila{
+    width: 30%;
+  }
+}
+
+.title-1{
+  font-style: italic;
+  font-weight: 500;
+  font-size: 45px;
+  line-height: 58px;
+  color: #B1B1B1;
+}
+
+.title-2{
+  font-style: italic;
+  font-weight: 500;
+  font-size: 45px;
+  line-height: 58px;
+  color: #FFFFFF;
+  margin-top:-2%;
+}
+
+@media (max-width:400px){
+  .title-2{
+    margin-top: -10%;
+  }
+}
+
+.sub-title{
+  font-weight: 500;
+  letter-spacing: 0.05em;
+  color: #B1B1B1;
+}
+
+.btn{
+  background: #FCE83A;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 0px 30px;
+  color: #B1B1B1;
+}
+
+.background{
+  background: rgba(255, 255, 255, 0.80);
+  border-radius: 10px;
+  width: 100%;
+}
 
 </style>
